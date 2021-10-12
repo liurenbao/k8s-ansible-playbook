49fb33b2fda4c4dbbe56a326632eb1d17d4a331e
liuhuan086 <liuhuan086@gmail.com>
2021-10-12 10:00:28 +0800

    Automated deployment

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 ansible-playbook -i hosts 01_init.yml
 ansible-playbook -i hosts 02_install_harbor.yml
 ansible-playbook -i hosts 04_install_etcd.yml
 ansible-playbook -i hosts 05_install_kube_apiserver.yml
 ansible-playbook -i hosts 06_install_master_proxy.yml
 ansible-playbook -i hosts 07_install_kube_related.yml
+ansible-playbook -i hosts 08_install_kubelet.yml
+ansible-playbook -i hosts 09_install_kube_proxy.yml
+ansible-playbook -i hosts 10_check_cluster.yml
