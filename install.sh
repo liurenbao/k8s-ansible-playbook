98b0a95325bb8517a886657a67ef3b637e666541
liuhuan086 <liuhuan086>
2021-10-22 13:04:57 +0800

    install ingress

--- install.sh
+++ install.sh
@@ -16,42 +16,48 @@ exit 1
 fi
 
 s_time=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i yml/00_init.yml"
-ansible-playbook -i hosts yml/00_init.yml
+echo "$(date "+%Y-%m-%d %H:%M:%S") 00_init.yml"
+ansible-playbook -i hosts ./yml/00_init.yml
 
 echo "$(date "+%Y-%m-%d %H:%M:%S") 初始化完成，重启机器中，需要等待一分钟"
 sleep 60
 echo "$(date "+%Y-%m-%d %H:%M:%S") 机器重启完成"
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts ./yml/01_install_bind_docker.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 01_install_bind_docker.yml"
 ansible-playbook -i hosts ./yml/01_install_bind_docker.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts ./yml/02_install_harbor.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 02_install_harbor.yml"
 ansible-playbook -i hosts ./yml/02_install_harbor.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 04_install_etcd.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 04_install_etcd.yml"
 ansible-playbook -i hosts ./yml/04_install_etcd.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 05_install_kube_apiserver.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 05_install_kube_apiserver.yml"
 ansible-playbook -i hosts ./yml/05_install_kube_apiserver.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 06_install_master_proxy.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 06_install_master_proxy.yml"
 ansible-playbook -i hosts ./yml/06_install_master_proxy.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 07_install_kube_related.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 07_install_kube_related.yml"
 ansible-playbook -i hosts ./yml/07_install_kube_related.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 08_install_kubelet.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 08_install_kubelet.yml"
 ansible-playbook -i hosts ./yml/08_install_kubelet.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 09_install_kube_proxy.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 09_install_kube_proxy.yml"
 ansible-playbook -i hosts ./yml/09_install_kube_proxy.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts 10_check_cluster.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 10_check_cluster.yml"
 ansible-playbook -i hosts ./yml/10_check_cluster.yml
 
-echo "$(date "+%Y-%m-%d %H:%M:%S") ansible-playbook -i hosts yml/11_install_flannel.yml"
+echo "$(date "+%Y-%m-%d %H:%M:%S") 11_install_flannel.yml"
 ansible-playbook -i hosts yml/11_install_flannel.yml
+
+echo "$(date "+%Y-%m-%d %H:%M:%S") 12_install_CoreDNS.yml"
+ansible-playbook -i hosts yml/12_install_CoreDNS.yml
+
+echo "$(date "+%Y-%m-%d %H:%M:%S") 13_install_ingress.yml"
+ansible-playbook -i hosts yml/13_install_ingress.yml
 
 e_time=$(date "+%Y-%m-%d %H:%M:%S")
 
